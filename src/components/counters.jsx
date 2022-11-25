2c4e0dc7f2cce47f945227a4ac94e0c1ef251149
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-11-25 15:25:36 +0000

    Reset button

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -19,15 +19,28 @@ class Counters extends Component {
         this.setState({ counters });
     }
 
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
+    };
+
     handleDelete = (counterId) => {
         const counters = this.state.counters.filter(c => c.id !== counterId);
         this.setState({ counters });
     };
 
     render() {
-
         return (
             <React.Fragment>
+                <button
+                    onClick={(this.handleReset)}
+                    className="btn btn-primary btn-sm m-2"
+                >
+                    Reset
+                </button>
                 {this.state.counters.map(counter =>
                     <Counter
                         key={counter.id}
